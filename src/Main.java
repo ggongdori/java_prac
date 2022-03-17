1fe12f7815179f5a21d901ac12fba23f8132364e
ggongdori <soominlee0410@gmail.com>
2022-03-17 14:39:07 +0900

    자바 기억 되살리기 프로젝트 day 1

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Back to Java");
+        for (int i = 0; i < ; i++) {
+
+        }
+        for (String arg : args) {
+
+        }
+        ArrayList<Integer> list = new ArrayList<>();
+
+
+
+    }
+}
